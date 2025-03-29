3a67d1e654ccda07b343541f4a6cfde667787217
IBS - Ilhas Business Software <ibsoftcv@gmail.com>
2025-03-29 12:32:23 -0100

    Update script-content.js

--- script-content.js
+++ script-content.js
@@ -1,7 +1,7 @@
 /**
  * ATTENTION !!! ATTENTION !!! ATTENTION
- * USE AT YOUR OWN RISK, MESSAGES DELETED FROM FACEBOOK MESSAGES, SOMETIMES CAN NOT BE RECOVERED
- * Script to delete all messages from my facebook account
+ * USE AT YOUR OWN RISK, MESSAGES DELETED FROM FACEBOOK MENSENGER CAN SYNC ON DEVICES, SOMETIMES CAN NOT BE RECOVERED
+ * Script to delete all friends conversations (not market) from my facebook account
  */
 
 (function () {
